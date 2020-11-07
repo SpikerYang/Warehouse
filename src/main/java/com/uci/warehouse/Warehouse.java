533323e144668bfa578490bfdca0e5e5d9bb9945
Jindong <jindongy@uci.edu>
2020-11-07 14:25:15 -0800

    add printroutemap for DP

--- src/main/java/com/uci/warehouse/Warehouse.java
+++ src/main/java/com/uci/warehouse/Warehouse.java
@@ -569,6 +569,7 @@ public class Warehouse {
             timePeriod = endTime-startTime;
             System.out.println("For approach 2, this order takes time around  "+ timePeriod + "  ms");
             printRoute(order, route, start, end);
+            printRouteMap(order,route,start,end);
             System.out.print("\n\n");
             //export direction to txt
             exportFile.exportTxt("\n\nOrder:1\n"+direction);
