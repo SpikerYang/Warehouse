12ceb8e2ad275e033a09007df20f3418fd48b377
Zheng_W <33081724+ZhengWang-Zayn@users.noreply.github.com>
2020-11-20 21:41:44 -0800

    Add files via upload

---
+++ src/main/java/com/uci/warehouse/Controller/OrderController.java
@@ -0,0 +1,47 @@
+package main.java.com.uci.warehouse.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import main.java.com.uci.warehouse.GUI.ViewCenter;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class OrderController implements Initializable{
+    private static final Logger logger = Logger.getLogger(OrderController.class.getName());
+    private ViewCenter viewCenter;
+
+    @FXML
+    private Button creat_new_order_button;
+
+    @FXML
+    private TextField login_username;
+
+    @FXML
+    private TextField login_password;
+
+
+    public void createNewOrderButtonClick(){
+        logger.log(Level.INFO, "User create a new order. Go to page createNewOrder.");
+        viewCenter.gotoCreateOrder();
+    }
+
+    public void oirderListButtonClick(){
+        logger.log(Level.INFO, "User try to get the order list. Go to order list createNewOrder.");
+        viewCenter.gotoOrderList();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void setApp(ViewCenter viewCenter){
+        this.viewCenter = viewCenter;
+    }
+}
