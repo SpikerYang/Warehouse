d529e41685c09267d98ca78f63f73261b6f9417f
Zheng_W <33081724+ZhengWang-Zayn@users.noreply.github.com>
2020-11-20 21:34:52 -0800

    Add files via upload

---
+++ Warehouse/src/main/java/com/uci/warehouse/Controller/CreateOrderController.java
@@ -0,0 +1,50 @@
+package main.java.com.uci.warehouse.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import main.java.com.uci.warehouse.GUI.ViewCenter;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CreateOrderController implements Initializable {
+
+    private static final Logger logger = Logger.getLogger(MapController.class.getName());
+    private ViewCenter viewCenter;
+
+    @FXML
+    private TextField new_order_filename;
+
+    @FXML
+    private TextField new_order_size;
+
+    @FXML
+    private TextField new_order_products;
+
+    @FXML
+    private TextField new_order_start_point;
+
+    @FXML
+    private TextField new_order_end_point;
+
+
+    public void createNewOrderButtonClick(){
+        if(true) {
+            logger.log(Level.INFO, "Create new order successful！ Go to Map page");
+            viewCenter.gotoMap();
+        } else {
+            logger.log(Level.WARNING, "something wrong.");
+        }
+    }
+    public void setApp(ViewCenter viewCenter){
+        this.viewCenter = viewCenter;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
