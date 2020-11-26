584ccd058e646ce5be83c120b85f5ab040e244e1
Jindong <jindongy@uci.edu>
2020-11-25 20:33:53 -0800

    add loadfile

---
+++ src/main/java/com/uci/warehouse/Controller/LoadFile.java
@@ -0,0 +1,14 @@
+package main.java.com.uci.warehouse.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoadFile {
+    @FXML
+    private Button login_button;
+    @FXML
+    private TextField OrderFileName;
+    @FXML
+    private TextField loctionFileName;
+}
